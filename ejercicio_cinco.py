c556da53fbec7a826990a130ec29de0c44daea13
ABEL JURADO <abel.jurado210399@gmail.com>
2024-07-17 11:10:22 -0500

    solucion ejercicio 5

--- ejercicio_cinco.py
+++ ejercicio_cinco.py
@@ -4,3 +4,16 @@ ejm:
 entrada: ("jose","alvarez","30","APSTI","III")
 salida: {nombre:"jose",apellido:"alvarez",edad:"30",programa_estudio:"APSTI",semestre:"III"}
 """
+def datos_personales_alumno(nombre, apellido, edad, grado, seccion):
+    alumno = {
+        "Nombre": nombre,
+        "Apellido": apellido,
+        "Edad": edad,
+        "Grado": grado,
+        "Sección": seccion
+    }
+    return alumno
+
+# Ejemplo de uso de la función
+datos_jose = datos_personales_alumno("Jose", "Alvarez", "30", "APSTI", "III")
+print(datos_jose)
