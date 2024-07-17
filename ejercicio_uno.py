50076a17bb83a953e615fb34cde29957fd1426fb
ABEL JURADO <abel.jurado210399@gmail.com>
2024-07-17 09:44:53 -0500

    solucion:ejercicio_1

--- ejercicio_uno.py
+++ ejercicio_uno.py
@@ -4,3 +4,16 @@ ejem:
 entrada: [4,7,10,4,1,0]
 salida : {menor:0,mayor:10}
 """
+
+def numero_menor_mayor(lista_numeros):
+    if not lista_numeros:
+        return {},{}
+    numero_menor=min(lista_numeros)
+    numero_mayor=max(lista_numeros)
+    return{"min":numero_menor,"max":numero_mayor}
+numero= [4,7,10,4,1,0]
+resultado=numero_menor_mayor(numero)
+print(resultado)
+
+
+
